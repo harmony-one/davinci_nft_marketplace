d4af24207372478ed2c7658e610eef6210cf5d5f
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2021-03-24 07:19:43 +0200

    Update 4_davinci_multiple_token_deploy.js

--- migrations/4_davinci_multiple_token_deploy.js
+++ migrations/4_davinci_multiple_token_deploy.js
@@ -5,7 +5,7 @@ module.exports = function(deployer) {
     return;
     const admin = "0x8AF062f0675f4C71c2b2b4aF44f16D28aE29dc98";
     return deployer
-      .deploy(DavinciMultipleToken, "Davinci", "DIEN", admin, "https://ipfs.io/ipfs/", "https://ipfs.io/ipfs/")
+      .deploy(DavinciMultipleToken, "DigitalEnergy", "DIEN", admin, "https://ipfs.io/ipfs/", "https://ipfs.io/ipfs/")
       .then(function(token) {
         console.log(`DigitalEnergyMultipleToken is deployed at ${token.address}`);
       });
